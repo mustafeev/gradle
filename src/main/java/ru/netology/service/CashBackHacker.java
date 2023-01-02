7351a8005138d0f08c89b586a5e9a9c14915211d
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2023-01-02 16:24:57 +0400

    initial commit

--- src/main/java/ru/netology/service/CashBackHacker.java
+++ src/main/java/ru/netology/service/CashBackHacker.java
@@ -1,8 +1,6 @@
 package ru.netology.service;
 
 public class CashBackHacker {
-
-    public class CashbackHackService {
         public int CashbackHackService(int amount) {
             if (amount < 1000) {
                 return 0;
@@ -14,4 +12,3 @@ public class CashBackHacker {
             return bonus;
         }
     }
-}
---
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+//import static org.testng.Assert.*;
+
+import static org.testng.Assert.assertEquals;
+//import static org.testng.annotations.Test.*;
+public class CashBackHackerTest {
+    @Test
+    void calculateBonus() {
+        int amount = 2000;
+        int expected =10;
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int actualCash = cashBackHacker.CashbackHackService(amount);
+        assertEquals(actualCash, expected);
+    }
+    @Test
+    void calculateIfAmount1000() {
+        int amount = 1000;
+        int expected =0;
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int actualCash = cashBackHacker.CashbackHackService(amount);
+        assertEquals(actualCash, expected);
+    }
+    @Test
+    void calculateIfAmountFewerThan1000() {
+        int amount = 996;
+        int expected =0;
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int actualCash = cashBackHacker.CashbackHackService(amount);
+        assertEquals(actualCash, expected);
+    }
+
+}
