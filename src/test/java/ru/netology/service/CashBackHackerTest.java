915d73ab6c324a86dd19aab591982522098aebb0
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2023-01-03 18:22:56 +0400

    maube final one

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Test;
 
 //import static org.testng.Assert.*;
 
-import static org.testng.Assert.assertEquals;
-//import static org.testng.annotations.Test.*;
+//import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.annotations.Test.*;
 public class CashBackHackerTest {
     @Test
     void calculateBonus() {
