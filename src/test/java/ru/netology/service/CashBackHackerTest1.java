70a4cec3bb560fbc1585b9fed5b3ac10af1b8852
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2023-01-12 19:35:47 +0400

    unknown commit

--- src/test/java/ru/netology/service/CashBackHackerTest1.java
+++ src/test/java/ru/netology/service/CashBackHackerTest1.java
@@ -1,47 +1,38 @@
 package ru.netology.service;
 
 import org.junit.After;
-import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeGroups;
 
-
-
-import static org.junit.Assert.*;
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashBackHackerTest1 {
 
     @Test
     public void cashbackHackService() {
-        //@BeforeSuite
-        //Я не понимаю почему ошибка в аннотации.
-        @AfterClass
-        void calculateBonus () {
-            int amount = 2000;
-            int expected =10;
-            CashBackHacker cashBackHacker = new CashBackHacker();
-            int actualCash = cashBackHacker.CashbackHackService(amount);
-            assertEquals(expected, actualCash);
-        }
-        @BeforeGroups
-        void calculateIfAmount1000 () {
-            int amount = 1000;
-            int expected =10;
-            CashBackHacker cashBackHacker = new CashBackHacker();
-            int actualCash = cashBackHacker.CashbackHackService(amount);
-            assertEquals(expected, actualCash);
-        }
-        @AfterSuite
-        void calculateIfAmountFewerThan1000 () {
-            int amount = 996;
-            int expected =0;
-            CashBackHacker cashBackHacker = new CashBackHacker();
-            int actualCash = cashBackHacker.CashbackHackService(amount);
-            assertEquals(expected, actualCash);
-        }
-
+        int amount = 2000;
+        int expected = 10;
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int actualCash = cashBackHacker.CashbackHackService(amount);
+        assertEquals(expected, actualCash);
     }
+    @After
+    public void calculateIfAmount1000() {
+        int amount = 1000;
+        int expected = 10;
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int actualCash = cashBackHacker.CashbackHackService(amount);
+        assertEquals(expected, actualCash);
+    }
+
+    @Before
+    public void calculateIfAmountFewerThan1000() {
+        int amount = 996;
+        int expected = 0;
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int actualCash = cashBackHacker.CashbackHackService(amount);
+        assertEquals(expected, actualCash);
+    }
+
 }
