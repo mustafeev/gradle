4505000e4c91b1702a1c2e5ac81e8cd3e5b3a475
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2023-01-17 21:46:41 +0400

    maybelast

---
+++ src/test/java/ru/netology/service/CashBackHackerTest1.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashBackHackerTest1 {
+
+    @Test
+    public void cashbackHackService() {
+        int amount = 2000;
+        int expected = 10;
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int actualCash = cashBackHacker.CashbackHackService(amount);
+        assertEquals(expected, actualCash);
+    }
+    @After
+    public void calculateIfAmount1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int actualCash = cashBackHacker.CashbackHackService(amount);
+        assertEquals(expected, actualCash);
+    }
+
+    @Before
+    public void calculateIfAmountFewerThan1000() {
+        int amount = 996;
+        int expected = 0;
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int actualCash = cashBackHacker.CashbackHackService(amount);
+        assertEquals(expected, actualCash);
+    }
+
+}
